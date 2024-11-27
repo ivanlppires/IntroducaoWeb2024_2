1314eac5988026e16634d0794b1ba773b7e788dc
ivanlppires <ivanpires@gmail.com>
2024-11-26 22:09:57 -0400

    Aula de 26-11-2024

---
+++ README.md
@@ -0,0 +1,1 @@
+# IntroducaoWeb2024_2
---
+++ ghub.sh
@@ -0,0 +1,7 @@
+echo "# IntroducaoWeb2024_2" >> README.md
+git init
+git add .
+git commit -m "Aula de 26-11-2024"
+git branch -M main
+git remote add origin https://github.com/ivanlppires/IntroducaoWeb2024_2.git
+git push -u origin main
